cfa412ed0dc6fe625677a93f7d8f52ff6688a890
glik <aggromarus@yandex.ru>
2023-09-08 16:20:07 +0300

    переделываю проект. По своему. Своими ручками, так сказать

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="header">
+    <div class="header__wrap container">
+      <div class="header__left">
+        <a href="index.html">
+          <img src="./assets/logo.png" alt="logo" />
+        </a>
+        <img src="./assets/search.png" alt="search" />
+      </div>
+      <div class="header__right">
+        <img src="./assets/burger.png" alt="burger__menu" />
+        <a href="vue_project/public/pages/registration.html"
+        ><img src="./assets/account.png" alt="account"
+        /></a>
+        <a href="vue_project/public/pages/cart.html"
+        ><img src="./assets/buy.png" alt="buy"
+        /></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: #222222;
+  color: aliceblue;
+  padding: 19px 0;
+
+  &__wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__left {
+    & img {
+      text-decoration: none;
+      margin-right: 41px;
+    }
+  }
+
+  &__right {
+    & img {
+      &:not(:last-child) {
+        margin-right: 33px;
+      }
+    }
+  }
+}
+</style>
