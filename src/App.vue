7dc98f636d0f721bc994b95770cf95dbb0bd5aae
glik <aggromarus@yandex.ru>
2023-09-08 16:31:13 +0300

    второй кусок верстки

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
 </script>
 
 <template>
+  <body>
   <div class="header">
     <div class="header__wrap container">
       <div class="header__left">
         <a href="index.html">
-          <img src="./assets/logo.png" alt="logo" />
+          <img src="./assets/logo.png" alt="logo"/>
         </a>
-        <img src="./assets/search.png" alt="search" />
+        <img src="./assets/search.png" alt="search"/>
       </div>
       <div class="header__right">
-        <img src="./assets/burger.png" alt="burger__menu" />
+        <img src="./assets/burger.png" alt="burger__menu"/>
         <a href="vue_project/public/pages/registration.html"
         ><img src="./assets/account.png" alt="account"
         /></a>
@@ -22,6 +23,208 @@
       </div>
     </div>
   </div>
+  <main>
+    <section class="promo">
+      <div class="promo__wrap container">
+        <div class="promo__title">
+          <h1>THE BRAND</h1>
+          <p>OF LUXERIOUS <span>FASHION</span></p>
+        </div>
+      </div>
+    </section>
+
+    <section class="category">
+      <div class="category__wrap container">
+        <div class="category__item">
+          <p>30% OFF <br/><span> FOR WOMEN</span></p>
+        </div>
+        <div class="category__item">
+          <p>HOT DEAL <br/><span> FOR MEN</span></p>
+        </div>
+        <div class="category__item">
+          <p>NEW ARRIVALS <br/><span> FOR KIDS</span></p>
+        </div>
+        <div class="category__item">
+          <p>LUXIROUS & TRENDY <br/><span>ACCESORIES</span></p>
+        </div>
+      </div>
+    </section>
+    <div class="featured">
+      <h3>Fetured Items</h3>
+      <p>Shop for items based on what we featured in this week</p>
+      <!-- указать переменные для <p> -->
+    </div>
+    <section class="product">
+      <div class="container">
+        <div class="product__wrap">
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="vue_project/public/pages/product.html"
+            ><img src="image/beard_man.png" alt="beard_man"
+            /></a>
+            <div class="card__text">
+              <h4>ELLERY X M'O CAPSULE</h4>
+              <p>
+                Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of cool Kym Ellery teams up with Moda
+                Operandi.
+              </p>
+              <div class="card__price">100500$</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="link">
+      <a class="link__browse" href="vue_project/public/pages/catalog.html"
+      >Browse All Product</a
+      >
+    </div>
+  </main>
+
+  <div class="comfort">
+    <div class="footer__wrap container">
+      <div class="comfort__item">
+        <img src="image/car.png" alt=""/>
+        <h4>Free Delivery</h4>
+        <p>
+          Worldwide delivery on all. Authorit tively morph next-generation
+          innov tion with extensive models.
+        </p>
+      </div>
+      <div class="comfort__item">
+        <img src="image/percent.png" alt=""/>
+        <h4>Sales & discounts</h4>
+        <p>
+          Worldwide delivery on all. Authorit tively morph next-generation
+          innov tion with extensive models.
+        </p>
+      </div>
+      <div class="comfort__item">
+        <img src="image/crown.png" alt=""/>
+        <h4>Quality assurance</h4>
+        <p>
+          Worldwide delivery on all. Authorit tively morph next-generation
+          innov tion with extensive models.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="footer__background">
+      <div class="container footer__wrap">
+        <div class="footer__info">
+          <img src="image/subs.png" alt="SUBSCRIBE"/>
+          <p>
+            “Vestibulum quis porttitor dui! Quisque viverra nunc mi, a
+            pulvinar purus condimentum“
+          </p>
+        </div>
+        <div class="footer__describe">
+          <p>SUBSCRIBE <br/>FOR OUR NEWLETTER AND PROMOTION</p>
+          <br/>
+          <br/>
+          <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Your Email"
+          />
+          <a href="#" id="subscribe">Subscribe</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer__bottom container">
+      <span>2021 Brand All Rights Reserved.</span>
+      <div class="links">
+        <a class="fab fa-facebook-square" href="https://www.facebook.com/">
+        </a>
+        <a class="fab fa-instagram" href="https://www.instagram.com/"></a>
+        <a
+            class="fab fa-pinterest-square"
+            href="https://www.pinterest.ru/"
+        ></a>
+        <a class="fab fa-twitter-square" href="https://twitter.com/"></a>
+      </div>
+    </div>
+  </div>
+  </body>
 </template>
 
 <style scoped>
